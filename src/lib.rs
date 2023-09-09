9390f1059b6cae84f108cd399e3bdef62349d1ff
aaronknudtson <aaron.knudtson@outlook.com>
2023-09-08 22:20:46 -0400

    initial working example

---
+++ src/lib.rs
@@ -0,0 +1,33 @@
+pub mod thread_pool {
+    use std::sync::Arc;
+    use std::thread::{self, JoinHandle};
+
+    pub fn do_the_thing<F>(
+        jobs: usize,
+        lines: &'static Vec<String>,
+        f: &'static F,
+        job: Vec<String>,
+    ) -> Vec<JoinHandle<Vec<Vec<u8>>>>
+    where
+        F: Fn(&'static String, &Vec<String>) -> Vec<u8> + Sync,
+    {
+        let n_lines = lines.len();
+        let jobs = if jobs == 0 || jobs > n_lines {
+            n_lines
+        } else {
+            jobs
+        };
+        let ajob = Arc::new(job);
+        lines
+            .chunks(n_lines / jobs)
+            .map(|chunk| {
+                let bjob = Arc::clone(&ajob);
+                thread::spawn(move || chunk.iter().map(|elem| f(elem, &bjob)).collect())
+            })
+            .collect()
+    }
+
+    pub fn max_par() -> usize {
+        return thread::available_parallelism().unwrap().get();
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,63 @@
+use clap::Parser;
+use fpar::thread_pool;
+use once_cell::unsync;
+use std::io::{self, Write};
+use std::process::Command;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None, trailing_var_arg = true)]
+struct Args {
+    /// Number of jobs
+    #[arg(short, long)]
+    jobs: Option<usize>,
+
+    /// job to run
+    job: Vec<String>,
+}
+
+fn the_thing(line: &'static String, command: &Vec<String>) -> Vec<u8> {
+    let proper_command: String = command.iter().fold(String::new(), |mut s, c| {
+        s.push_str(" ");
+        s.push_str(match c.as_str() {
+            "{}" => line,
+            _ => c,
+        });
+        s
+    });
+    let res = Command::new("sh").arg("-c").arg(&proper_command).output().expect("deez nuts broken");
+    res.stdout
+}
+
+static mut INPUTS: unsync::Lazy<Vec<String>> = unsync::Lazy::new(|| Vec::new());
+
+fn main() {
+    let mut args = Args::parse();
+    let input = io::stdin();
+
+    // read from stdin
+    input
+        .lines()
+        .for_each(|l| unsafe { INPUTS.push(l.unwrap()) });
+    if !args.job.iter().any(|v| v.as_str()=="{}") {
+        args.job.push(String::from("{}"));
+    }
+
+    // start threads to run the function
+    let threads = thread_pool::do_the_thing(
+        args.jobs.unwrap_or(thread_pool::max_par()),
+        unsync::Lazy::get(unsafe { &INPUTS }).unwrap(),
+        &the_thing,
+        args.job,
+    );
+
+    // collect threads to print to stdout
+    for thread in threads {
+        let res = thread.join().expect("Thread failed");
+
+        res.iter().for_each(|output| {
+            io::stdout()
+                .write(output.as_slice())
+                .expect("can't write to stdout");
+        })
+    }
+}
